3c427fdd76cf72d8da07a139dc3b21942c1270b1
poovasokan <poovasokan@gmail.com>
2023-12-30 18:38:04 +0000

    UPDATED TYPO

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 //20.Importing the Cart Context Provider
-import { CartProvider } from './context/cart.jsx'
+import { CartProvider } from './context/Cart.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
